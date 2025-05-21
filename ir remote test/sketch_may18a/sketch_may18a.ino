77510ca85f2901ddb40195fa94a7ccf7ef1a21ba
YokaiS <82001107+seanoyama@users.noreply.github.com>
2025-05-21 07:43:53 -0700

    balancing code

--- ir remote test/sketch_may18a/sketch_may18a.ino
+++ ir remote test/sketch_may18a/sketch_may18a.ino
@@ -1,19 +1,23 @@
 #include <IRremote.h>
 
 #include <Servo.h>
-#include <LED.h>
+// #include <LED.h>
 // #include <iostream>
 
 int pos = 0;
 int ifRead;
 int toggleValue = 0;
-int motor1_fwdpin = 2;
-int motor1_backpin = 3;
-int enablepin = 5;
+int motor1_fwdpin = 13;
+int motor1_backpin = 12;
+int motor1_enablepin = 5;
 
-int irReceiverPin = 4;
-IRrecv irrecv(irReceiverPin);
-decode_results results;
+int motor2_fwdpin = 11;
+int motor2_backpin = 10;
+int motor2_enablepin = 4;
+
+// int irReceiverPin = 4;
+// IRrecv irrecv(irReceiverPin);
+// decode_results results;
 
 
 Servo m_servo;
@@ -22,44 +26,61 @@ Servo m_servo;
 void setup(){
   Serial.begin(9600);
   // pinMode(A0, INPUT);
-  // m_servo.attach(2);
+  // m_servo.attach(7);
   // m_servo.write(90);
-  irrecv.enableIRIn();
+  // irrecv.enableIRIn();
   pinMode(motor1_fwdpin,OUTPUT);
   pinMode(motor1_backpin,OUTPUT);
-  pinMode(enablepin,OUTPUT);
+  pinMode(motor1_enablepin,OUTPUT);
+  
+  pinMode(motor2_fwdpin,OUTPUT);
+  pinMode(motor2_backpin,OUTPUT);
+  pinMode(motor2_enablepin,OUTPUT);
 
-  analogWrite(enablepin, 150);
+  analogWrite(motor1_enablepin, 150);
   digitalWrite(motor1_fwdpin, HIGH);
   digitalWrite(motor1_backpin, LOW);
+  
+  analogWrite(motor2_enablepin, 150);
+  digitalWrite(motor2_fwdpin, HIGH);
+  digitalWrite(motor2_backpin, LOW);
 }
 
 void loop(){
   
 
-  // if(irrecv.decode(&results)){
-  //     if(results.value == 5316027){
-  //       Serial.println("Go Forward");
-  //       digitalWrite(motor1_fwdpin, HIGH);
-  //       digitalWrite(motor1_backpin, LOW);
-  //       // m_servo.write(45);
-  //     }
-  //     else if(results.value == 2747854299){
-  //       Serial.println("Go Backward");
-  //       digitalWrite(motor1_fwdpin, LOW);
-  //       digitalWrite(motor1_backpin, HIGH);
-  //       // m_servo.write(135);
-  //     }
-  //     else if(results.value == 3622325019){
-  //       Serial.println("idle");
-  //       digitalWrite(motor1_fwdpin, LOW);
-  //       digitalWrite(motor1_backpin, LOW);
-  //       // m_servo.write(90);
-  //     }
+  if(irrecv.decode(&results)){
+      if(results.value == 5316027){
+        Serial.println("Go Forward");
+        // analogWrite(enablepin, 150);
+        digitalWrite(motor1_fwdpin, HIGH);
+        digitalWrite(motor1_backpin, LOW);
+        digitalWrite(motor2_fwdpin, HIGH);
+        digitalWrite(motor2_backpin, LOW);
+        // m_servo.write(45);
+      }
+      else if(results.value == 2747854299){
+        Serial.println("Go Backward");
+        // analogWrite(enablepin, 150);
+        digitalWrite(motor1_fwdpin, LOW);
+        digitalWrite(motor1_backpin, HIGH);
+        digitalWrite(motor2_fwdpin, LOW);
+        digitalWrite(motor2_backpin, HIGH);
+        // m_servo.write(135);
+      }
+      else if(results.value == 3622325019){
+        Serial.println("idle");
+        // analogWrite(enablepin, 0);
+        digitalWrite(motor1_fwdpin, LOW);
+        digitalWrite(motor1_backpin, LOW);
+        digitalWrite(motor2_fwdpin, LOW);
+        digitalWrite(motor2_backpin, LOW);
+        // m_servo.write(90);
+      }
 
-  //     irrecv.resume();
-  //     delay(100);
-  // }
+      irrecv.resume();
+      delay(100);
+  }
   
   
 }
